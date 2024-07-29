5ee0c08df10a16ab46342313fdc5df4eeed994e1
Deepc <deepanshu.chalia96@gmail.com>
2024-07-29 18:26:49 -0500

    Setup firebase

--- app/page.js
+++ app/page.js
@@ -1,9 +1,31 @@
+'use client'
 import { Box, Stack, Typography } from "@mui/material";
+import { collection, getDocs, query } from "@firebase/firestore";
+import { useEffect, useState } from "react";
+import { firestore } from "@/firebase";
 
-const items = ['tomato', 'potato', 'onion', 'garlic', 'ginger', 'chicken', 'beef', 'pork', 'fish', 'milk', 'egg', 'butter', 'cheese', 'yogurt', 'bread', 'rice', 'pasta', 'flour', 'sugar', 'salt', 'pepper', 'oil', 'vinegar', 'soy sauce', 'ketchup', 'mustard', 'mayonnaise', 'honey', 'jam', 'peanut butter', 'coffee', 'tea', 'juice', 'soda', 'beer', 'wine', 'whiskey', 'vodka']
+
+// const item = ['tomato', 'potato', 'onion', 'garlic', 'ginger', 'chicken', 'beef', 'pork', 'fish', 'milk', 'egg', 'butter', 'cheese', 'yogurt', 'bread', 'rice', 'pasta', 'flour', 'sugar', 'salt', 'pepper', 'oil', 'vinegar', 'soy sauce', 'ketchup', 'mustard', 'mayonnaise', 'honey', 'jam', 'peanut butter', 'coffee', 'tea', 'juice', 'soda', 'beer', 'wine', 'whiskey', 'vodka']
 
 
 export default function Home() {
+  const [pantry, setPantry] = useState([])
+
+  useEffect(() => {
+    const updatePantry = async () => {
+      const snapshot = query(collection(firestore, 'pantry'))
+      const docs = await getDocs(snapshot)
+      const pantryList = []
+      docs.forEach((doc) => {
+        console.log(doc.id, doc.data());
+        pantryList.push(doc)
+      })
+      console.log(pantryList);
+      setPantry(pantryList)
+    }
+    updatePantry()
+  }, [])
+
   return (
     <Box 
       width="100vw" 
@@ -13,51 +35,52 @@ export default function Home() {
       alignItems={'center'}  
       flexDirection={'column'}
     >
-      <Box 
-        width={'800px'} 
-        height={'100px'} 
-        bgcolor={"#89CFF0"} 
-        display={'flex'} 
-        justifyContent={'center'} 
-        alignContent={'center'}
-        border={'1px solid #333'}
-      >
-        <Typography 
-          variant="h2" 
-          color={'#333'} 
-          textAlign={'center'}
+      <Box border={'1px solid #333'}>
+        <Box 
+          width={'800px'} 
+          height={'100px'} 
+          bgcolor={"#89CFF0"} 
+          display={'flex'} 
+          justifyContent={'center'} 
+          alignContent={'center'} 
         >
-          Pantry Items
-        </Typography>
+          <Typography 
+            variant="h2" 
+            color={'#333'} 
+            textAlign={'center'}
+          >
+            Pantry Items
+          </Typography>
       </Box>  
-      <Stack 
-        width="800px"
-        height="400px"
-        spacing={2}
-        overflow={'scroll'}
-      >
-        {items.map((i) => (
-          <Box 
-            key={i}
-            width={"100%"}
-            height={"100px"}
-            display={"flex"}
-            justifyContent={'center'}
-            alignItems={'center'}
-            bgcolor={"#f0f0f0"}
-          >  
-            <Typography
-              variant="h3"
-              color={"#333"}
-              textAlign={'center'}
-            >
-              {
-                i.charAt(0).toUpperCase() + i.slice(1)
-              }
-            </Typography>
-          </Box>
-        ))}
-      </Stack>
+        <Stack 
+          width="800px"
+          height="400px"
+          spacing={2}
+          overflow={'scroll'}
+        >
+          {pantry.map((p) => (
+            <Box 
+              key={i}
+              width={"100%"}
+              height={"70px"}
+              display={"flex"}
+              justifyContent={'center'}
+              alignItems={'center'}
+              bgcolor={"#f0f0f0"}
+            >  
+              <Typography
+                variant="h3"
+                color={"#333"}
+                textAlign={'center'}
+              >
+                {
+                  p.charAt(0).toUpperCase() + p.slice(1)
+                }
+              </Typography>
+            </Box>
+          ))}
+        </Stack>
+      </Box>
     </Box>
   );
 }
---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDM3mG1qMFpbNPTwpXS0OZe4SzzXenCYKk",
+  authDomain: "dcpantryapp.firebaseapp.com",
+  projectId: "dcpantryapp",
+  storageBucket: "dcpantryapp.appspot.com",
+  messagingSenderId: "168113441178",
+  appId: "1:168113441178:web:16505fd7706dc1d2fa5223"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app)
+export {app, firestore}
